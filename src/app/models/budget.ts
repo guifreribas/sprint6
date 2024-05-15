f5935b5d7d97619ef5d11758701c234fa56dcdea
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-16 00:01:46 +0200

    Exercici 2

--- src/app/models/budget.ts
+++ src/app/models/budget.ts
@@ -1,1 +1,24 @@
-export type Checkbox = 'seoCheckbox' | 'adsCheckbox' | 'webCheckbox';
+import { WritableSignal } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+// export type BudgetCheckboxItems = 'seoCheckbox' | 'adsCheckbox' | 'webCheckbox';
+// export type BudgetNumberItems = 'languages' | 'pages';
+
+export interface UpdateCheckboxParams {
+  checkbox?: string;
+  numberInput?: string;
+}
+
+export interface TotalBudgetParams {
+  budget: FormGroup;
+  pages: WritableSignal<number>;
+  languages: WritableSignal<number>;
+}
+
+interface MyForm {
+  seoCheckbox: FormControl<boolean | null>;
+  adsCheckbox: FormControl<boolean | null>;
+  webCheckbox: FormControl<boolean | null>;
+  languages: FormControl<number>;
+  pages: FormControl<number>;
+}
--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal } from '@angular/core';
+// import { TotalBudgetParams } from '../models/budget';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BudgetService {
+  constructor() {}
 
-  constructor() { }
+  //Nombre de pàgines * el nombre d'idiomes * 30)€.
+  getTotalWeb(
+    pages: number,
+    languages: number
+    // pages: WritableSignal<number>,
+    // languages: WritableSignal<number>
+  ): number {
+    return pages * languages * 30 + 500;
+  }
+
+  getTotalBudget({
+    budget,
+    pages,
+    languages,
+  }: {
+    budget: FormGroup;
+    pages: number;
+    languages: number;
+    // pages: WritableSignal<number>;
+    // languages: WritableSignal<number>;
+  }): number {
+    let total = 0;
+
+    if (budget.controls['seoCheckbox'].value) {
+      total += 300;
+    }
+    if (budget.controls['adsCheckbox'].value) {
+      total += 400;
+    }
+    if (budget.controls['webCheckbox'].value) {
+      total += this.getTotalWeb(pages, languages);
+    }
+    console.log(total);
+    return total;
+  }
 }
