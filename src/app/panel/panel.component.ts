8658788519e317fcb32343161b8a2863af28a7fa
Guifré Puig Ribas <guifreribas@gmail.com>
2024-05-16 00:01:46 +0200

    Exercici 2

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-panel',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, ReactiveFormsModule, CommonModule],
   templateUrl: './panel.component.html',
-  styleUrl: './panel.component.scss'
+  styleUrl: './panel.component.scss',
 })
 export class PanelComponent {
+  @Input() budget!: FormGroup;
+  @Input() onChangeInputs!: () => void;
 
+  constructor() {}
+
+  addPage() {
+    this.budget.controls['pagesFrm'].setValue(
+      Number(this.budget.controls['pagesFrm'].value) + 1
+    );
+    this.onChangeInputs();
+  }
+  removePage() {
+    if (Number(this.budget.controls['pagesFrm'].value) > 0) {
+      this.budget.controls['pagesFrm'].setValue(
+        Number(this.budget.controls['pagesFrm'].value) - 1
+      );
+      this.onChangeInputs();
+    }
+  }
+  addLanguage() {
+    this.budget.controls['languagesFrm'].setValue(
+      Number(this.budget.controls['languagesFrm'].value) + 1
+    );
+    this.onChangeInputs();
+  }
+  removeLanguage() {
+    if (Number(this.budget.controls['languagesFrm'].value) > 0) {
+      this.budget.controls['languagesFrm'].setValue(
+        Number(this.budget.controls['languagesFrm'].value) - 1
+      );
+      this.onChangeInputs();
+    }
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BudgetService {
+  constructor() {}
 
-  constructor() { }
+  //Nombre de pàgines * el nombre d'idiomes * 30)€.
+  getTotalWeb(pages: number, languages: number): number {
+    return pages * languages * 30 + 500;
+  }
+
+  getTotalBudget({
+    budget,
+    pages,
+    languages,
+  }: {
+    budget: FormGroup;
+    pages: number;
+    languages: number;
+  }): number {
+    let total = 0;
+    if (typeof pages !== 'number') pages = 0;
+    if (typeof languages !== 'number') languages = 0;
+
+    if (budget.controls['seoCheckbox'].value) {
+      total += 300;
+    }
+    if (budget.controls['adsCheckbox'].value) {
+      total += 400;
+    }
+    if (budget.controls['webCheckbox'].value) {
+      total += this.getTotalWeb(pages, languages);
+    }
+    return total;
+  }
 }
