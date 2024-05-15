06592a94bf0dd1e323824e8419592784b1168c30
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-16 00:01:46 +0200

    Exercici 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,43 +1,61 @@
-import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Checkbox } from '../models/budget';
+import { Component, ViewChild, effect, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PanelComponent } from '../panel/panel.component';
+import { BudgetService } from '../services/budget.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PanelComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   public totalBudget = signal<number>(0);
+  public budgetItems = signal<Record<string, number>[]>([]);
+  public isWebChecked = signal<boolean>(false);
 
-  public budget = new FormGroup({
+  public theBoundOnChange = () => {};
+
+  public budget: FormGroup = new FormGroup({
     seoCheckbox: new FormControl(false),
     adsCheckbox: new FormControl(false),
     webCheckbox: new FormControl(false),
+    languagesFrm: new FormControl(0, Validators.pattern('^[0-9]*$')),
+    pagesFrm: new FormControl(0, Validators.pattern('^[0-9]*$')),
   });
 
-  private budgetPrices: Record<string, number> = {
-    seoCheckbox: 300,
-    adsCheckbox: 400,
-    webCheckbox: 500,
-  };
+  constructor(private budgetService: BudgetService) {}
 
-  constructor() {}
-
-  updateCheckbox(id: Checkbox) {
-    this.budget.controls[id].setValue(!this.budget.controls[id].value);
-    this.calculateBudget(id);
-  }
-
-  calculateBudget(id: Checkbox) {
-    if (this.budget.controls[id].value) {
-      this.totalBudget.update((prev) => prev + this.budgetPrices[id]);
-    } else {
-      this.totalBudget.update((prev) => prev - this.budgetPrices[id]);
+  addWebItem() {
+    this.isWebChecked.update((prev) => !prev);
+    if (!this.isWebChecked()) {
+      this.budget.controls['languagesFrm'].setValue(0);
+      this.budget.controls['pagesFrm'].setValue(0);
     }
   }
 
-  ngOnInit(): void {}
+  onChangeInputs() {
+    console.log({ budget: this.budget });
+    const result = this.budgetService.getTotalBudget({
+      budget: this.budget,
+      pages: this.budget.controls['pagesFrm'].value,
+      languages: this.budget.controls['languagesFrm'].value,
+    });
+    this.totalBudget.set(result);
+  }
+
+  ngOnInit(): void {
+    this.budget.valueChanges.subscribe(() => {
+      this.onChangeInputs();
+    });
+
+    this.theBoundOnChange = this.onChangeInputs.bind(this);
+  }
 }
