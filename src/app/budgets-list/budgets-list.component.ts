8ed8ce97aba34b0d94d1808ba8b553e4f0424671
Guifré Puig Ribas <guifreribas@gmail.com>
2024-05-17 22:34:36 +0200

    Exercici 7

--- src/app/budgets-list/budgets-list.component.ts
+++ src/app/budgets-list/budgets-list.component.ts
@@ -2,19 +2,34 @@ import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { BudgetService } from '../services/budget.service';
 import { CommonModule } from '@angular/common';
-import { SortedAction } from '../models/budget';
+import { Budget, SortedAction } from '../models/budget';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-budgets-list',
   standalone: true,
-  imports: [MatIconModule, CommonModule],
+  imports: [MatIconModule, CommonModule, FormsModule],
   templateUrl: './budgets-list.component.html',
   styleUrl: './budgets-list.component.scss',
 })
 export class BudgetsListComponent {
   public sortAction = signal<SortedAction>({ action: 'INIT', type: 'ASC' });
+  public filteredBudgets = signal<Budget[]>([]);
+  public inputValue: string = '';
 
   constructor(public budgetService: BudgetService) {}
+
+  filterBudgetsByName(value: string) {
+    if (value === '') {
+      this.filteredBudgets.set([]);
+      return;
+    }
+    this.filteredBudgets.set(
+      this.budgetService
+        .budgets()
+        .filter((budget) => budget.name.includes(value))
+    );
+  }
 
   sortByDate() {
     const actionType = this.sortAction().type;
